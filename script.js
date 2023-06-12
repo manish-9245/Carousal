3a57bc0ece0883bf4a5e8e33927bfcc44847e8f5
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-06-12 11:21:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const wrapper = document.querySelector('.wrapper');
+const images = wrapper.querySelectorAll('img');
+
+let isTransitioning = false;
+
+// Clone the first image and append it to the end
+const firstImage = images[0].cloneNode(true);
+wrapper.appendChild(firstImage);
 
 wrapper.addEventListener('scroll', () => {
+  if (isTransitioning) return;
+
   const scrollWidth = wrapper.scrollWidth;
   const scrollLeft = wrapper.scrollLeft;
   const clientWidth = wrapper.clientWidth;
   
   if (scrollLeft === scrollWidth - clientWidth) {
-    const firstImage = wrapper.querySelector('img');
-    wrapper.appendChild(firstImage.cloneNode(true));
-    firstImage.remove();
-    wrapper.scrollLeft -= clientWidth;
+    isTransitioning = true;
+
+    wrapper.style.scrollBehavior = 'auto';
+    wrapper.scrollLeft = 0;
+
+    setTimeout(() => {
+      wrapper.style.scrollBehavior = 'smooth';
+      wrapper.appendChild(images[0].cloneNode(true));
+      images[0].remove();
+      isTransitioning = false;
+    }, 100);
   }
 });
