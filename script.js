0c7d8c415066bd5f8b78334523eed315f54e5711
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-06-12 11:28:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 const wrapper = document.querySelector('.wrapper');
 const images = wrapper.querySelectorAll('img');
 
-const firstImage = images[0];
-const lastImage = images[images.length - 1];
-
-// Clone the first and last images
-const clonedFirstImage = firstImage.cloneNode(true);
-const clonedLastImage = lastImage.cloneNode(true);
-
-// Append the cloned images before/after the original images
-wrapper.insertBefore(clonedLastImage, firstImage);
-wrapper.appendChild(clonedFirstImage);
-
 wrapper.addEventListener('scroll', () => {
   const scrollWidth = wrapper.scrollWidth;
   const scrollLeft = wrapper.scrollLeft;
   const clientWidth = wrapper.clientWidth;
 
-  if (scrollLeft === 0) {
-    wrapper.scrollLeft = scrollWidth - 2 * clientWidth;
-  } else if (scrollLeft === scrollWidth - clientWidth) {
-    wrapper.scrollLeft = clientWidth;
+  if (scrollLeft === scrollWidth - clientWidth) {
+    const clones = Array.from(images).map(image => image.cloneNode(true));
+    clones.forEach(clone => wrapper.appendChild(clone));
+  } else if (scrollLeft === 0) {
+    const clones = Array.from(images).map(image => image.cloneNode(true));
+    clones.reverse().forEach(clone => wrapper.insertBefore(clone, images[0]));
+    wrapper.scrollLeft = scrollWidth - clientWidth;
   }
 });
