f0a3230b7f3fcc92b902a01dec19e4e5def2007a
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-06-12 11:44:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const wrapper = document.querySelector('.wrapper');
 const images = wrapper.querySelectorAll('img');
-const cloneCount = 3; // Number of clones to add
+const cloneCount = 3; // Number of clones to pre-render
+
+// Initial clones
+for (let i = 0; i < cloneCount; i++) {
+  const clone = images[i].cloneNode(true);
+  wrapper.appendChild(clone);
+}
 
 wrapper.addEventListener('scroll', () => {
   const scrollWidth = wrapper.scrollWidth;
@@ -8,16 +14,32 @@ wrapper.addEventListener('scroll', () => {
   const clientWidth = wrapper.clientWidth;
   const totalImages = images.length;
 
-  if (scrollLeft >= (totalImages - cloneCount) * clientWidth) {
+  if (scrollLeft >= scrollWidth - clientWidth * (cloneCount + 1)) {
     for (let i = 0; i < cloneCount; i++) {
       const clone = images[i].cloneNode(true);
       wrapper.appendChild(clone);
     }
   }
-});
 
-// Initial clones
-for (let i = 0; i < cloneCount; i++) {
-  const clone = images[totalImages - 1 - i].cloneNode(true);
-  wrapper.insertBefore(clone, images[0]);
-}
+  // Check if there are enough clones pre-rendered ahead
+  const preRenderedClones = wrapper.querySelectorAll('.clone');
+  if (preRenderedClones.length < cloneCount) {
+    const lastImageIndex = totalImages - cloneCount;
+    const startIndex = lastImageIndex - preRenderedClones.length;
+    for (let i = startIndex; i < lastImageIndex; i++) {
+      const clone = images[i].cloneNode(true);
+      clone.classList.add('clone');
+      wrapper.insertBefore(clone, images[0]);
+    }
+    wrapper.scrollLeft += clientWidth * preRenderedClones.length;
+  }
+
+  // Check if clones need to be removed from the end
+  const clonesToRemove = wrapper.querySelectorAll('.clone');
+  if (clonesToRemove.length > cloneCount) {
+    for (let i = 0; i < cloneCount; i++) {
+      wrapper.removeChild(clonesToRemove[i]);
+    }
+    wrapper.scrollLeft -= clientWidth * cloneCount;
+  }
+});
